ef949a72e5ce8a99277f3d623ec62bff77827431
Jelmarose <jelmarosegracedevera@gmail.com>
2025-04-06 19:30:21 +0800

    added bookings, terminals and timetables module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { TicketsController } from './tickets/tickets.controller';
 import { TicketsModule } from './tickets/tickets.module';
 import { CabinsModule } from './cabins/cabins.module';
+import { TimetablesModule } from './timetables/timetables.module';
+import { TerminalsModule } from './terminals/terminals.module';
+import { BookingsModule } from './bookings/bookings.module';
 
 @Module({
-  imports: [TicketsModule, CabinsModule],
+  imports: [TicketsModule, CabinsModule, TimetablesModule, TerminalsModule, BookingsModule],
   controllers: [AppController],
   providers: [AppService],
 })
